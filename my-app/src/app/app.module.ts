b6cd26c8c9e28ead60011b46706cfdc96e51fc0e
Foyaz-Ahmed <ahmedfoyaz0000@gmail.com>
2022-06-18 13:00:51 +0600

    split individual component from app and call from app

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServerElementComponent } from './server-element/server-element.component';
+import { PcElementComponent } from './pc-element/pc-element.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServerElementComponent,
+    PcElementComponent,
+    CockpitComponent
   ],
   imports: [
     BrowserModule,
---
+++ my-app/src/app/pc-element/pc-element.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pc-element',
+  templateUrl: './pc-element.component.html',
+  styleUrls: ['./pc-element.component.css']
+})
+export class PcElementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
